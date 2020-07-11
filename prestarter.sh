af4976b0aafb908e69c97dd8d865983502b17f29
phozzy <arthur.fayzullin@gmail.com>
2020-07-11 09:01:14 +0000

    Add prestarting configuration script

---
+++ prestarter.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+gcloud config set project dinsurance
+gcloud services enable compute.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable container.googleapis.com
